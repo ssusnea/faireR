6fd01a44b028691a860ca6db210c3abc777308de
Sarah Susnea <ssusnea@smith.edu>
2025-08-20 04:04:51 -0400

    testing the branches/pr workflow

--- R/calculate_scores.R
+++ R/calculate_scores.R
@@ -1,3 +1,1 @@
-# this will be the function that calculates the scores for the data
-# and returns a "fairness matrix" that can be plotted
-# depending on the number of dimensions
+# this is a scrap sheet
