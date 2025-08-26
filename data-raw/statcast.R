a87c0d4bb157dc3ff8c5983a8b3337e588f14f67
Ben Baumer <ben.baumer@gmail.com>
2025-08-26 10:12:05 -0400

    documented data

--- data-raw/statcast.R
+++ data-raw/statcast.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 pitches <- read_csv(here::here("data-large/statcast_pitch_swing_data_20240402_20240630.csv"))
 
-pitches <- pitches |>
+csas25 <- pitches |>
   select(type, sz_top, sz_bot, plate_z, plate_x, stand)
 
-usethis::use_data(pitches)
+usethis::use_data(csas25)
