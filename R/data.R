c683985b41487c92b4057cb004608b6baa43c8a8
Sarah Susnea <ssusnea@smith.edu>
2025-08-27 15:53:22 -0400

    first steps to adding ironman data

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Ironman Texas race results from 2024
+#'
+#' @description Data from the 2024 Ironman Texas race
+#' @details
+#' Contains data for professional racers' times and rankings as well as
+#' predicted outcomes derived from paper author's proposed quotient method
+#'
+#' @docType data
+#' @source <https://www.coachcox.co.uk/imstats/race/2148/results/>
+"ironman"
