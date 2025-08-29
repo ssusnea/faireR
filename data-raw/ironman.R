a6bbd31e440a1a1a9fc5376de6d5d19a42e68cad
Sarah Susnea <ssusnea@smith.edu>
2025-08-29 15:39:53 -0400

    trying edits per code review

--- data-raw/ironman.R
+++ data-raw/ironman.R
@@ -20,4 +20,25 @@ ironman <- marathon |>
          y_hat = as.factor(y_hat)
         )
 
+female_wr <- hms("8:10:34")
+male_wr <- hms("7:24:20")
+
+# idk if this worked.... even though it seems like it should?
+
+ironman2 <- marathon |>
+  select(Gender, Division, `Division Rank`, `Overall Time`, `Finish Status`) |>
+  filter(Division %in% c("MPRO", "FPRO") & `Finish Status` == "Finisher") |>
+  mutate(`Quotient Model` = case_when(Gender == "Female" ~ hms(`Overall Time`)/female_wr,
+                                      Gender == "Male" ~ hms(`Overall Time`)/male_wr),
+         Yhat = case_when(Gender == "Male" & dense_rank(`Quotient Model`) <= 10 ~ 1,
+                       Gender == "Female" & dense_rank(`Quotient Model`) <= 7 ~ 1,
+                       .default = 0)
+         )
+
+
+  # group_by(Gender) |>
+  # mutate(y = ifelse(`Division Rank` <= 10, 1, 0),
+  #        y_hat = ifelse(dense_rank(desc(`Quotient Model`)) <= 10, 1, 0)) |>
+  # ungroup()
+
 usethis::use_data(ironman, overwrite = TRUE)
