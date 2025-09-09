a9a1ed049b8e6c84e73dfe4de6bdaa80fb934a49
Ben Baumer <ben.baumer@gmail.com>
2025-09-09 10:00:02 -0400

    added fairness_cube()

--- R/yardstick.R
+++ R/yardstick.R
@@ -69,3 +69,38 @@ fairness_predictive_parity <- function(data, truth, estimate) {
 #' @export
 #' @rdname fairness_cv
 fairness_sufficiency <- fairness_predictive_parity
+
+#' @export
+#' @rdname fairness_cv
+#' @examplesIf require(dplyr)
+#' # Compute fairness measures for Ironman
+#' ironman |>
+#'   group_by(gender) |>
+#'   fairness_cube(truth = y, estimate = y_hat)
+fairness_cube <- function(data, truth, estimate) {
+  msr <- yardstick::metric_set(
+    yardstick::detection_prevalence,
+    yardstick::sens,
+    yardstick::spec,
+    yardstick::ppv,
+    yardstick::npv
+  )
+
+  data |>
+    msr(obs, truth = {{ truth }}, estimate = {{ estimate }}) |>
+    dplyr::mutate(
+      .estimate = ifelse(.metric == "spec", 1 - .estimate, .estimate),
+      coordinate = dplyr::case_when(
+        grepl("prevalence", .metric) ~ "independence",
+        grepl("sens|spec", .metric) ~ "separation",
+        grepl("pv", .metric) ~ "sufficiency",
+        .default = as.character("")
+      )
+    ) |>
+    dplyr::group_by(gender, coordinate) |>
+    dplyr::summarize(
+      calder_weavers = abs(diff(.estimate)),
+      equalized_odds = abs(diff(.estimate)),
+      predictive_parity = abs(diff(.estimate))
+    )
+}
